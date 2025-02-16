51fbbde294ad1b193b4876a2820e9a6d71d2bbc1
Lurldgbodex <professionaleditor6@gmail.com>
2025-02-16 19:15:56 +0100

    chore: update interface and improve functionality

--- frontend/hngx-stage2/src/App.tsx
+++ frontend/hngx-stage2/src/App.tsx
@@ -1,18 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar/Navbar"
 import TicketSelection from "./components/TicketSelection/TicketSelection"
+import AttendeeDetails from "./components/AttendeeDetails/AttendeeDetails"
+import TicketReady from "./components/TicketReady/TicketReady"
 
 function App() {
 
   return (
-    <Router>
+    <div className="scaffold">
+      <Router>
       <Navbar />
       <Routes>
         <Route path="/" element={<TicketSelection />} />
-        {/* <Route path='/form' element={<AttendeeForm />} /> */}
-        {/* <Route path="/ticket" element={<TicketPreview} /> /> */}
+        <Route path='/form' element={<AttendeeDetails />} />
+        <Route path="/ticket" element={<TicketReady /> } />
       </Routes>
     </Router>
+    </div>
   )
 }
 
